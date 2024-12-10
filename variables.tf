e01eeeb48a498f7fb8f56442204ea94b6791d167
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2024-12-10 10:08:06 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,16 +4,15 @@ variable "subscription_id" {
   default ="0d9c3010-f797-440d-89aa-cce65153833b"
 }
 
-variable "DOCKER_REGISTRY_SERVER_USER" [
+variable "DOCKER_REGISTRY_SERVER_USER" {
   description = "DOCKER_REGISTRY_SERVER_USER"
    type        = string
   default ="jesstg"
-]
+}
 
 
-variable "DOCKER_REGISTRY_SERVER_PASSWORD" [
+variable "DOCKER_REGISTRY_SERVER_PASSWORD" {
   description = "DOCKER_REGISTRY_SERVER_PASSWORD"
    type        = string
   default ="Jess2001@@"
-]
-
+}
