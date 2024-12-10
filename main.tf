5188446a3488a6ac740b9c2677f0bba9675658b0
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2024-12-10 10:11:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  subscription_id=var.subscription_id
 }
 
 resource "azurerm_resource_group" "example" {
@@ -12,8 +12,8 @@ resource "azurerm_app_service_plan" "example" {
   name                = "example-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  kind                = "Linux"
-  reserved            = true
+  kind                = "Linux" # Change to Linux for Docker container support
+  reserved            = true    # Required for Linux App Service Plans
 
   sku {
     tier = "Basic"
@@ -28,47 +28,52 @@ resource "azurerm_app_service" "example" {
   app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    linux_fx_version = "DOCKER|jesstg/petclinic:latest"
+    linux_fx_version = "DOCKER|jesstg/petclinic:latest" # Specify the Docker image
   }
 
   app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "true"
-    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io/v1/"
-    "DOCKER_REGISTRY_SERVER_USER"         = var.DOCKER_REGISTRY_SERVER_USER
-    "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.DOCKER_REGISTRY_SERVER_PASSWORD
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "true" # Required for containerized apps
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io/v1/" # Optional for public Docker images
+    "DOCKER_REGISTRY_SERVER_USER"         = var.DOCKER_REGISTRY_SERVER_USER # DockerHub username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.DOCKER_REGISTRY_SERVER_PASSWORD # DockerHub password
   }
 }
 
-#------------------------------------------------------
-#    App Insights + Log Analytics + Diagnostics
-#------------------------------------------------------
+
+
+#------------------------------------------------------###################
+#    App Insights + Log Analytics + Diagnostics 
+#------------------------------------------------------###################
 
 resource "azurerm_application_insights" "example" {
-  name                = "example-appinsights"
+  name                = "tf-test-appinsights-stage"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  application_type    = "java"
-  workspace_id        = azurerm_log_analytics_workspace.example.id
+  application_type    = "java" #java web app
+  workspace_id        = azurerm_log_analytics_workspace.rg.id
 }
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                       = "log-analytics-example"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
+# Azure Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "rg" {
+  name                       = "log-analytics"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
   sku                        = "PerGB2018"
   retention_in_days          = 30
-  internet_ingestion_enabled = false
+  tags                       = null
+  internet_ingestion_enabled = false #best practice to disable public access
 }
 
-data "azurerm_monitor_diagnostic_categories" "example" {
-  resource_id = azurerm_app_service.example.id
+data "azurerm_monitor_diagnostic_categories" "main" {
+  resource_id = azurerm_linux_web_app.app.id
 }
 
+# Connecter le Azure Web App au Log Analytics Workspace
+#Activer qqes paramètres de diagnostic de l'application
 resource "azurerm_monitor_diagnostic_setting" "example" {
   name                       = "webapp-diagnostics"
-  target_resource_id         = azurerm_app_service.example.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-
+  target_resource_id         = azurerm_linux_web_app.app.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.rg.id
   enabled_log {
     category = "AppServiceHTTPLogs"
     retention_policy {
@@ -106,57 +111,49 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
   }
 }
 
-locals {
+ locals {
   solution_name = toset([
-    "Security",
-    "SecurityInsights",
-    "AgentHealthAssessment",
-    "AzureActivity",
-    "SecurityCenterFree",
-    "DnsAnalytics",
-    "ADAssessment",
-    "AntiMalware",
-    "ServiceMap",
-    "SQLAssessment",
-    "SQLAdvancedThreatProtection",
-    "Updates"
+    "Security", "SecurityInsights", "AgentHealthAssessment", "AzureActivity", "SecurityCenterFree", "DnsAnalytics", "ADAssessment", "AntiMalware", "ServiceMap", "SQLAssessment", "SQLAdvancedThreatProtection", "Updates"
   ])
 }
 
-resource "azurerm_log_analytics_solution" "example" {
+resource "azurerm_log_analytics_solution" "solutions" {
   for_each              = local.solution_name
   solution_name         = each.key
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  workspace_resource_id = azurerm_log_analytics_workspace.example.id
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.rg.id
+  workspace_name        = azurerm_log_analytics_workspace.rg.name
 
   plan {
     publisher = "Microsoft"
     product   = "OMSGallery/${each.key}"
   }
-}
+} 
 
-#------------------------------------------------------
+
+#------------------------------------------------------###################
 #    Mail Alert
-#------------------------------------------------------
+#------------------------------------------------------###################
 
-resource "azurerm_monitor_action_group" "example" {
-  name                = "example-action-group"
-  resource_group_name = azurerm_resource_group.example.name
-  short_name          = "alert-example"
 
-  email_receiver {
-    email_address = "narjes.taghlet@insat.ucar.tn"
-    name          = "Monitoring Alert"
-  }
+resource "azurerm_monitor_action_group" "monitor" {
+  name                = "actiongroup"
+  resource_group_name = azurerm_resource_group.rg.name
+  short_name          = "alert1234"
+ email_receiver {
+   email_address = "narjes.taghlet@insat.ucar.tn"
+   name = "Monitoring Alert ! "
+ }
 }
 
-resource "azurerm_monitor_metric_alert" "example" {
-  name                = "example-metric-alert"
-  resource_group_name = azurerm_resource_group.example.name
-  scopes              = [azurerm_app_service.example.id]
-  description         = "Metric alert for web app"
-  enabled             = true
+# define criteria
+resource "azurerm_monitor_metric_alert" "metric" {
+  name                = "metricalert-webapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  scopes              = [azurerm_linux_web_app.frontwebapp.id]
+  description         = "alerts"
+  enabled = true
 
   criteria {
     metric_namespace = "Microsoft.Web/sites"
@@ -167,6 +164,7 @@ resource "azurerm_monitor_metric_alert" "example" {
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.example.id
+    action_group_id = azurerm_monitor_action_group.monitor.id
   }
-}
+  }
+
